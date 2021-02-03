6e7de98cadf5a388827d915a81ba9117212ef542
Henrik Nero <henriknero@gmail.com>
2021-02-03 16:49:25 +0000

    Tweaked script or something

--- dockerscript.sh
+++ dockerscript.sh
@@ -1,12 +1,16 @@
 #! /bin/sh
+docker pull binhex/arch-delugevpn
+docker build . -t deluge-image
+docker stop deluge
+docker rm deluge
 
 docker run -d \
     --name=deluge \
     --cap-add=NET_ADMIN \
-    -p 8112:8112 \
+    -p 127.0.0.1:8112:8112 \
     -e LAN_NETWORK=192.168.122.0/24 \
-    -v /home/henrik/Media/data:/data \
+    -v /home/nero/Media/data:/data \
     -e PUID=1000 \
     -e PGID=1000 \
-   -v /config/delugeconfigfiles:/config/
-    deluge
+    -v /home/nero/deluge-container/config:/config/ \
+    deluge-image
